7cf3b3208f48e89ae77a0298e40c18a90e2cd35d
deepscan <demodeepscan@gmail.com>
2019-01-25 10:51:19 +0900

    test20190125
    test

--- karma.conf.js
+++ karma.conf.js
@@ -165,5 +165,18 @@ function addSauceTests (cfg) {
 
   // for slow browser booting, ostensibly
   cfg.captureTimeout = 120000;
+  
+  function CWE_398() { // IDENTICAL_BRANCHES
+    if (x >= 0) {
+        y = x;
+    } else {
+        y = x;
+    }
+}
+function CWE_476() { // NULL_POINTER
+    var obj;
+    var y = obj.x;  // UNINITIALIZED_LOCAL_VAR
+    console.log(y);
+}
   cfg.browserNoActivityTimeout = 20000;
 }
